f82c1a3caf68a6a6b3a1790ff5e55fba9c2d051d
Siddhant <siddhant.shah.1986@gmail.com>
2020-01-26 03:41:10 +0530

    Cricinfo_Scrapping - Added Scoreboard

--- Web Scraping - BeautifulSoup/Cricinfo_Scrapping/cricinfo_web_scrapper.py
+++ Web Scraping - BeautifulSoup/Cricinfo_Scrapping/cricinfo_web_scrapper.py
@@ -2,9 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 # url fo the match
-base_url = 'https://www.espncricinfo.com/series/19286/game/1185307/south-africa-vs-england-4th-test-icc-world-test-championship-2019-2021'
 
-resp = requests.get(base_url)   # fetching data from the url
+url = 'https://www.espncricinfo.com/series/19430/game/1183531/australia-vs-pakistan-2nd-test-icc-world-test-championship-2019-2021'
+
+url = url.split('game')
+
+match_url =  url[0] + 'game' + url[1]
+scorecard_url =  url[0] + 'scorecard' + url[1]
+
+resp = requests.get(match_url)   # fetching data from the match url
 if resp.status_code == 200:     # executing further only if we have successfully fetched data from the url
     soup = BeautifulSoup(resp.text, 'html.parser')      # parsing data into beautifulsoup object using html parser
 
@@ -40,8 +46,8 @@ if resp.status_code == 200:     # executing further only if we have successfully
     print(score_note)
     print()
 
-    # score summary for live match
-    if match_status == 'Live':
+    # FETCHING SCORE SUMMARY FOR LIVE PAST MATCH
+    if match_status in ['Live', 'Lunch', 'Tea', 'Stumps']:
 
         scoreboard_summary_block = soup.find('table')   # fetching html 'table' which holds scoreboard summary
         batsman_header_row = scoreboard_summary_block.find('thead')     # fetching 1st 'thead' element which holds header row for batsman
@@ -127,7 +133,10 @@ if resp.status_code == 200:     # executing further only if we have successfully
             # if 'tr' element count is 3 the we fetch partnership_tag and recent_ball_tag and break out of loop
             if bowler_counter > 2:
                 partnership_tag = bowler_row.next_sibling
-                recent_ball_tag = partnership_tag.next_sibling
+                if match_detail[4:8] == 'Test':
+                    recent_ball_tag = partnership_tag.next_sibling.next_sibling
+                else:
+                    recent_ball_tag = partnership_tag.next_sibling
                 break
 
         print()
@@ -141,15 +150,149 @@ if resp.status_code == 200:     # executing further only if we have successfully
         for balls in ball_update.next_siblings:
             print(balls.get_text() + ' ', end='')
 
-    # score summary for match played in past
+    # FETCHING SCORE SUMMARY FOR THE PAST MATCH
     elif match_status == 'Result':
-        print('Scoreboard')
-        scoreboard = soup.find('article', class_=['sub-module', 'scorecard'])
-        print(scoreboard.name)
+        match_heading_block = soup.find('article', class_=['top-stories'])
+        try:
+            print(match_heading_block.h1.get_text())
+            print(match_heading_block.p.get_text().replace('"', ''))
+        except expression as identifier:
+            pass
+        
+        print()
+        
+        scoreboard_summary_block = soup.find('article', class_=['scorecard-summary'])
+        scoreboard_summary_heading = scoreboard_summary_block.find('header', class_='bordered')
+        print(scoreboard_summary_heading.get_text())
 
-    print()
+        # Team's Score Summary:
+        for team in scoreboard_summary_heading.next_sibling.children:
+            scoreboard_summary_Team = team.h4.get_text()
+            print(f'{scoreboard_summary_Team}')
+            for batsmen_summary in team.find_all('ul'):
+                for batsmen in batsmen_summary.find_all('li'):
+                    batsmen_name = batsmen.a.get_text()
+                    batsmen_score = batsmen.span.get_text()
+                    print(f'\t{batsmen_name}\t{batsmen_score}')
+                print()
+
+    print('\n'*1)
+
+    # FETCHING SCORECARD FOR THE MATCH
+    resp_match = requests.get(scorecard_url)    # fetching data from the match scorecard url
+
+    if resp_match.status_code == 200:   # executing further only if we have successfully fetched data from the scorecard url
+        soup_scorecard = BeautifulSoup(resp_match.text, 'html.parser')      # parsing data into beautifulsoup object using html parser
+
+        innings = soup_scorecard.find_all('article', class_='scorecard')    # fetching list of all inings in the match
+
+        for inning in innings:  # looping through all inings inthe match
+            inning_title = inning.div      # fetching the 1st div element for that particular innings that holds title for innings
+            inning_details = inning_title.next_sibling     # the very next sibling tag holds all information fo the inning
+
+            # fetching all children (div for batsmen and bowlers) for this particluar tag 
+            inning_details_children = inning_details.children  # inning_details_children = [<div class="scorecard-section batsmen">, <div class="scorecard-section bowler">
+            
+            print(inning_title.get_text())
+            print()
+
+            # looping through 
+            for inning_details in inning_details_children:
+                
+                player_number = 1
+                
+                # fetching datat for the batsman for this innings in the match
+                if inning_details['class'][1] == 'batsmen':
+                    
+                    # fetching rows of all batsmen
+                    all_batsmen = inning_details.children   # all_batsmen = <div class="flex-row">
+                    
+                    dnb = ''
+
+                    # looping through each batsman
+                    for batsman in all_batsmen:     # batsman = [<div class="wrap header">, <div class="wrap batsmen">] for 1st iteration and batsman = <div class="wrap batsmen"> for 2nd iteration
+
+                        # fetching details of extras conceded by bowling side
+                        if batsman.find('div').get('class') == ['wrap', 'extras']:
+                            data_heading = batsman.find('div', class_='cell')
+                            data_value = data_heading.next_sibling
+                            extras = data_heading.get_text() + ': ' + data_value.get_text()
+
+                        # fetching details of total runs scored by batting side
+                        elif batsman.find('div').get('class') == ['wrap', 'total']:
+                            data_heading = batsman.find('div', class_='cell')
+                            data_value = data_heading.next_sibling
+                            total = data_heading.get_text() + ': ' + data_value.get_text()
+
+                        # fetching details of fall of wickets and players yest to bat
+                        elif batsman.find('div').get('class') == ['wrap', 'dnb']:
+                            for dnb_data in batsman.find_all('div', class_='dnb'):
+                                dnb = dnb + '\n' + dnb_data.get_text()
+
+                        # printing scorecard for batsmsn
+                        else:
+                            # printing details for the every 1st batsmen, segrating heading for batsmen scorecard and batsman score details
+                            if player_number == 1:
+                                # printing header for batman
+                                batsman_header = batsman.find('div', class_='header')
+
+                                for header in batsman_header.children:   # printing headers for batsman
+                                    if header['class'][1] == 'batsmen':
+                                        print(header.get_text(), end='\t\t')
+                                    elif header['class'][1] == 'commentary':
+                                        print(' \t', end='\t\t')
+                                    else:
+                                        print(header.get_text(), end='\t')
+                                
+                                print()
+
+                                # printing details for batsman
+                                for details in batsman_header.next_sibling.children:   
+                                    if details['class'][1] == 'batsmen':
+                                        print(details.get_text(), end='\t')
+                                    elif details['class'][1] == 'commentary':
+                                        print(details.get_text(), end='\t')
+                                    else:
+                                        print(details.get_text(), end='\t')
+                            
+                            # printing details for every other batsman
+                            else:
+                                for details in batsman.find('div', class_='batsmen').children:
+                                    if details['class'][1] == 'batsmen':
+                                        print(details.get_text(), end='\t')
+                                    elif details['class'][1] == 'commentary':
+                                        print(details.get_text(), end='\t')
+                                    else:
+                                        print(details.get_text(), end='\t')
     
+                            print()
+                        
+                        player_number = player_number + 1
+
+                    print(extras + '\t' + total + dnb)
+                    print()
+
+                # fetching datat for the bowlers for this innings in the match
+                elif inning_details['class'][1] == 'bowling':
+                    
+                    headings = inning_details.find('thead').tr.children     # fetching element for headings of bowlere
+
+                    # printing headings
+                    for heading in headings:
+                        print(heading.get_text(), end='\t')
+                    
+                    print()
+                    body = inning_details.find('tbody').children    # fetching element for details of bowlers
+                    
+                    # printing details of each bowler
+                    for bowler in body:
+                        for bowler_detail in bowler.children:
+                            print(bowler_detail.get_text(), end='\t')
+                        print()
+            
+            print('\n'*1)
+    
+    else:
+        print('Error')
 else:
     print('Error')
-
-print()
